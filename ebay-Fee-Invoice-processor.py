4e7289dc758a21483d2d197041716edd7840e571
Jacob Fifield <43118015+GermanWaffles@users.noreply.github.com>
2021-04-27 07:14:10 -0600

    Update ebay-Fee-Invoice-processor.py
    Added the invoice number and date range to the sanitized list
    Broke file reading section into the function fileReader
    removed some unnecessary c-style variable casting in main()

--- ebay-Fee-Invoice-processor.py
+++ ebay-Fee-Invoice-processor.py
@@ -3,18 +3,19 @@ adFees = []
 sellerFees = []
 
 def sanitize(tempList):
-    del tempList[0:6]
+    del tempList[0:3]
+    del tempList[2]
     return tempList
 
 def parseDateRange(tempList):
     tempDateRange = ""
-    for entries in tempList[4]:
+    for entries in tempList[1]:
         tempDateRange += entries
         tempDateRange = tempDateRange.replace('"','')
     return tempDateRange
 
 def parseFees(tempList):
-    for entry in tempList:
+    for entry in tempList[2:-1]:
         if (entry[6] == "Promoted listing fees"):
             adFees.append((entry[0]+entry[1], entry[5], entry[6], entry[-1]))
         else:
@@ -25,29 +26,27 @@ def generateReport(tempDateRange):
     tempSellerTotal =0
     for entry in adFees: tempAdTotal = tempAdTotal+float(entry[-1])
     for entry in sellerFees: tempSellerTotal = tempSellerTotal+float(entry[-1])
-    return (tempAdTotal, tempSellerTotal, tempDateRange)
+    return (round(tempAdTotal,2), round(tempSellerTotal,2), tempDateRange)
 
 def printReport(tempList):
     print("Total Spent on Ads during "+tempList[2])
     print(tempList[0])
-    print("Final value Fees + Insertion Fees during "+tempList[2])
+    print("Total Spent on Final value Fees and Insertion Fees during "+tempList[2])
     print(tempList[1])
 
-def main():
-    rawList = []
-    sanitizedList = []
-    adTotal = 0
-    sellerTotal = 0
-    dateRange = ""
-
+def fileReader():
     #Writes CSV contents to a list 
+    rawList = []
     with open('C:/Users/Fife/Documents/Fife-Electronics/Ebay-Invoices/invoiceId-6069749409.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in reader:
             rawList.append(row)
+    return rawList
 
-    dateRange = parseDateRange(rawList)
+def main():
+    rawList = fileReader()
     sanitizedList = sanitize(rawList)
+    dateRange = parseDateRange(sanitizedList)
     parseFees(sanitizedList)
     report = generateReport(dateRange)
     printReport(report)

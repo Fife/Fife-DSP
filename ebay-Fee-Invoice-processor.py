ee6ec5bc70d25776934fb8c340c798c87d38dac6
Jacob Fifield <43118015+GermanWaffles@users.noreply.github.com>
2021-04-27 11:01:53 -0600

    Update ebay-Fee-Invoice-processor.py
    Renamed fileReader() to CSVToList()
    CSVToList() is more boilerplate, taking a string as a file path and returning a list of comma separated values with comma as delimiter 
    Removed global variables
    sanitize() now returns a new list rather than mutating the raw list
    parseFees() now returns a list rather than mutating global variables

--- ebay-Fee-Invoice-processor.py
+++ ebay-Fee-Invoice-processor.py
@@ -1,11 +1,11 @@
 import csv
-adFees = []
-sellerFees = []
 
 def sanitize(tempList):
-    del tempList[0:3]
-    del tempList[2]
-    return tempList
+    newList = []
+    for item in tempList: newList.append(item)
+    del newList[0:3]
+    del newList[2]
+    return newList
 
 def parseDateRange(tempList):
     tempDateRange = ""
@@ -15,17 +15,20 @@ def parseDateRange(tempList):
     return tempDateRange
 
 def parseFees(tempList):
+    adFees = []
+    sellerFees = []
     for entry in tempList[2:-1]:
         if (entry[6] == "Promoted listing fees"):
             adFees.append((entry[0]+entry[1], entry[5], entry[6], entry[-1]))
         else:
             sellerFees.append((entry[0]+entry[1], entry[5], entry[6], entry[-1]))
+    return (adFees, sellerFees)
 
-def generateReport(tempDateRange):
+def generateReport(tempDateRange, tempFeeList):
     tempAdTotal=0 
     tempSellerTotal =0
-    for entry in adFees: tempAdTotal = tempAdTotal+float(entry[-1])
-    for entry in sellerFees: tempSellerTotal = tempSellerTotal+float(entry[-1])
+    for entry in tempFeeList[0]: tempAdTotal = tempAdTotal+float(entry[-1])
+    for entry in tempFeeList[1]: tempSellerTotal = tempSellerTotal+float(entry[-1])
     return (round(tempAdTotal,2), round(tempSellerTotal,2), tempDateRange)
 
 def printReport(tempList):
@@ -34,23 +37,21 @@ def printReport(tempList):
     print("Total Spent on Final value Fees and Insertion Fees during "+tempList[2])
     print(tempList[1])
 
-def fileReader():
+def CSVToList(filePath):
     #Writes CSV contents to a list 
     rawList = []
-    with open('C:/Users/Fife/Documents/Fife-Electronics/Ebay-Invoices/invoiceId-6069749409.csv', newline='') as csvfile:
+    with open(filePath, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in reader:
             rawList.append(row)
     return rawList
 
 def main():
-    rawList = fileReader()
+    filePath = 'C:/Users/Fife/Documents/Fife-Electronics/Ebay-Invoices/invoiceId-6069749409.csv'
+    rawList = CSVToList(filePath)
     sanitizedList = sanitize(rawList)
     dateRange = parseDateRange(sanitizedList)
-    parseFees(sanitizedList)
-    report = generateReport(dateRange)
+    feeList = parseFees(sanitizedList)
+    report = generateReport(dateRange, feeList)
     printReport(report)
 main()
-    
-
-

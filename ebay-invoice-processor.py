27cec62273994002c4e3cd5d009e1d53a38f48a6
Jacob Fifield <43118015+GermanWaffles@users.noreply.github.com>
2021-04-26 21:25:13 -0600

    Update ebay-invoice-processor.py
    started adding sanitation function
    started the sorting logic with extraction of the "Promoted Listing Fees"
    Currently, the program outputs the total cost spent on advertising 
    Need to add the rest of the sorting logic and extract the Ad rate from the notes section

--- ebay-invoice-processor.py
+++ ebay-invoice-processor.py
@@ -2,13 +2,27 @@ import csv
 
 rawList = []
 sanitizedList = []
+adFees = []
+sellerFees = []
+adTotal = 0
 
+#Writes CSV contents to a list 
 with open('C:/Users/Fife/Documents/Fife-Electronics/Ebay-Invoices/invoiceId-6069749409.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in reader:
         rawList.append(row)
 
+def sanitize(tempList):
+    del tempList[0:6]
+    return tempList
 
-for entries in rawList:
-    entries[0] = entries[0].replace('"', '')
-    print(entries)
+sanitizedList = sanitize(rawList)
+for entry in sanitizedList:
+    if (entry[6] == "Promoted listing fees"):
+        adFees.append((entry[0]+entry[1], entry[5], entry[6], entry[-1]))
+    else:
+        1+1 #toDo: Add Seller Fees Logic
+
+for entry in adFees: adTotal = adTotal+float(entry[-1])
+print(adTotal)
+    
